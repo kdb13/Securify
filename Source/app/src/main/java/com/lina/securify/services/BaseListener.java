7f80a647662d20df0d7c2f6fe0ce034a87b02708
Kdb13 <kanbhalani@gmail.com>
2020-01-07 22:49:18 +0530

    Added AccessibilityService.

---
+++ Source/app/src/main/java/com/lina/securify/services/BaseListener.java
@@ -0,0 +1,56 @@
+package com.lina.securify;
+
+import android.content.Context;
+import android.telecom.Call;
+import android.view.KeyEvent;
+
+/**
+ * The base class for listening to KeyEvents and performing a task
+ * based on some event logic.
+ */
+public abstract class BaseListener implements KeyEvent.Callback {
+
+    protected int keyCode;
+
+    protected Runnable task;
+
+    /**
+     * Must be used by the sub-classes to set the common parameters
+     * @param keyCode The KeyEvent with keyCode upon which the listener will listen
+     * @param task The task to perform on success
+     */
+    public BaseListener(int keyCode, Runnable task) {
+        this.keyCode = keyCode;
+        this.task = task;
+    }
+
+    /**
+     * This method must be called by the client to dispatch the event to this class.
+     *
+     * @param event
+     * The KeyEvent to be dispatched
+     */
+    public void dispatch(KeyEvent event) {
+        event.dispatch(this, new KeyEvent.DispatcherState(), this);
+    }
+
+    public interface Callback {
+
+        /**
+         * It is called when the listening is completed and is a success
+         */
+        public void onSucess();
+
+        /**
+         * It is called when listening is started.
+         */
+        public void onStartedListening();
+
+        /**
+         * It is called when listening is completed. To check for success
+         * implement the onSucess() method instead.
+         */
+        public void onCompletedListening();
+
+    }
+}
---
+++ Source/app/src/main/java/com/lina/securify/services/VolumeLongPressListener.java
@@ -0,0 +1,42 @@
+package com.lina.securify;
+
+import android.content.Context;
+import android.media.AudioManager;
+
+public class VolumeLongPressListener extends LongPressListener {
+
+    private VolumeResetter volumeResetter;
+
+    private boolean isSuccess = false;
+
+    public VolumeLongPressListener(Context context, int keyCode, int durationInMillis, Runnable task) {
+        super(keyCode, durationInMillis, task);
+
+        volumeResetter = new VolumeResetter(context, AudioManager.STREAM_RING);
+    }
+
+    @Override
+    public void onStartedListening() {
+        super.onStartedListening();
+
+        // Set the initial volume
+        volumeResetter.setInitialVolume();
+    }
+
+    @Override
+    public void onSucess() {
+        super.onSucess();
+
+        isSuccess = true;
+    }
+
+    @Override
+    public void onCompletedListening() {
+        super.onCompletedListening();
+
+        if (isSuccess) {
+            volumeResetter.reset();
+            isSuccess = false;
+        }
+    }
+}
