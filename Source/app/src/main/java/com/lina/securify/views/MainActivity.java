ea2f7b728053ed74a998c7358f3d118e998f5267
Kdb13 <kanbhalani@gmail.com>
2020-01-19 11:39:33 +0530

    Added menu with "Log out".

--- Source/app/src/main/java/com/lina/securify/views/MainActivity.java
+++ Source/app/src/main/java/com/lina/securify/views/MainActivity.java
@@ -2,9 +2,14 @@ package com.lina.securify.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.lina.securify.R;
+import com.lina.securify.views.auth.AuthActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,4 +22,25 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void logOut(MenuItem menuItem) {
+
+        if (menuItem.getItemId() == R.id.item_log_out) {
+
+            FirebaseAuth
+                    .getInstance()
+                    .signOut();
+
+            Intent intent = new Intent(this, AuthActivity.class);
+            startActivity(intent);
+
+            finish();
+        }
+
+    }
 }
