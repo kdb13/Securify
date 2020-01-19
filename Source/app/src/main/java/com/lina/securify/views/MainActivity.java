dc11d99cb0742b532238af65eba07bf1ac9650bb
Kdb13 <kanbhalani@gmail.com>
2020-01-19 17:34:21 +0530

    Added NavDrawer and Toolbar.

--- Source/app/src/main/java/com/lina/securify/views/MainActivity.java
+++ Source/app/src/main/java/com/lina/securify/views/MainActivity.java
@@ -1,12 +1,21 @@
 package com.lina.securify.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHost;
+import androidx.navigation.NavHostController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.lina.securify.R;
 import com.lina.securify.views.auth.AuthActivity;
@@ -15,17 +24,32 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = "MainActivity";
 
+    private AppBarConfiguration appBarConfiguration;
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        setupNavigation();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host);
+
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 
     public void logOut(MenuItem menuItem) {
@@ -42,5 +66,22 @@ public class MainActivity extends AppCompatActivity {
             finish();
         }
 
+    }
+
+    private void setupNavigation() {
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host);
+
+        appBarConfiguration =
+                new AppBarConfiguration.Builder(navController.getGraph())
+                        .setDrawerLayout(drawerLayout)
+                        .build();
+
+        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
+
+        NavigationView navView = findViewById(R.id.nav_view);
+        NavigationUI.setupWithNavController(navView, navController);
     }
 }
